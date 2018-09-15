536baaa9655b6c532b09606fb3a4756ccea08fde
Israel Caxile <israel.caxile@gmail.com>
2018-09-15 16:15:20 -0300

    feat: Cria Docs nas funcoes do Helper

--- src/Helper.js
+++ src/Helper.js
@@ -1,9 +1,19 @@
+/**
+ * @description Retorna string recebido em formato camel case
+ * @param {string} texto String em qualquer formato
+ * @returns {string} String no formato camel case
+ */
 export function toCamelCase(str) {
   return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
     return index === 0 ? letter.toLowerCase() : letter.toUpperCase();
   }).replace(/\s+/g, '');
 }
 
+/**
+ * @description Componente que só exibe children se passar no teste
+ * @param {boolean} teste
+ * @param {*} children
+ */
 export function If({ test, children }) {
   return test ? children : false;
 }
