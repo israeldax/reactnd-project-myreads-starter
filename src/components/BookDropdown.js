6f705c713482030dee81dbff80274b45a69ecfd4
Israel Caxile <israel.caxile@gmail.com>
2018-09-19 23:24:15 -0300

    feat: Adiciona negrito no dropdown de book

--- src/components/BookDropdown.js
+++ src/components/BookDropdown.js
@@ -5,10 +5,10 @@ function BookDropdown({ estado, changeShelf }) {
   return (
     <select value={estado} onChange={changeShelf} >
       <option value="move" disabled>Move to...</option>
-      <option value="currentlyReading">Currently Reading</option>
-      <option value="wantToRead">Want to Read</option>
-      <option value="read">Read</option>
-      <option value="none">None</option>
+      <option value="currentlyReading" className={estado === 'currentlyReading'? 'chose' : ''}>Currently Reading</option>
+      <option value="wantToRead" className={estado === 'wantToRead'? 'chose' : ''}>Want to Read</option>
+      <option value="read" className={estado === 'read'? 'chose' : ''}>Read</option>
+      <option value="none" className={estado === 'none'? 'chose' : ''}>None</option>
     </select>
   )
 }
